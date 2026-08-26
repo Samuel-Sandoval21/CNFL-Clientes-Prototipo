7da50b7b4d85c0000645dd08402fe735e8d44801
Samuel Sandoval <ssandoval40716@ufide.ac.cr>
2026-08-26 09:22:15 -0600

    Estructura base del proyecto ASP.NET MVC - CNFL Clientes Prototipo
    Creación del proyecto con .NET Framework 4.8.1, MVC, sin autenticación.
    Estructura inicial de carpetas y archivos de configuración.

---
+++ CNFL Clientes Prototipo/CNFL Clientes Prototipo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CNFL_Clientes_Prototipo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CNFL Clientes Prototipo/CNFL Clientes Prototipo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNFL_Clientes_Prototipo.MvcApplication" Language="C#" %>
